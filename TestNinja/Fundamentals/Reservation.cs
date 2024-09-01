90573c587cfcab2ffe803ef9871158e154e037ce
Ilham Binyetov <ilhamgb@code.edu.az>
2024-09-01 14:41:03 +0400

    Refactor the Reservation Class

--- TestNinja/Fundamentals/Reservation.cs
+++ TestNinja/Fundamentals/Reservation.cs
@@ -6,11 +6,8 @@
 
         public bool CanBeCancelledBy(User user)
         {
-            if(user.IsAdmin) return true;
-            if(MadeBy == user) return true;
-
-
-            return false;
+            return (user.IsAdmin || MadeBy == user) ;
+            
         }
         
     }
